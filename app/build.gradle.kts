6475ff3fbb5ce1d1cb2b81c53fcd42ab6a274636
Sangho Kim <marchbreeze03@gmail.com>
2025-01-14 23:48:22 +0900

    Update TargetSDK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.march.breeze"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.march.breeze"
         minSdk = 28
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
